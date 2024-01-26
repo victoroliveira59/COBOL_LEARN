ca53d33683d9793a20836edb87059bc031a0b501
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-26 19:19:09 -0300

    "Organização de arquivos"

---
+++ CALCULO_DE_JUROS/CALCULO_JUROS.cbl
@@ -0,0 +1,22 @@
+      *****************************************************************
+      * Program name:    MYPROG                               
+      * Original author: MYNAME                                
+      *
+      * Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 MYNAME  Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  CALCULO-JUROS.
+
+       ENVIRONMENT DIVISION. 
+        CONFIGURATION SECTION. 
+        SOURCE-COMPUTER. IBM-3081. 
+        OBJECT-COMPUTER. IBM-3081. 
+        INPUT-OUTPUT SECTION.
+        FILE-CONTROL. 
+         SELECT INFILE ASSIGN  TO INFILEDD.
+         SELECT OUTFILE ASSIGN TO OUTFILEDD.
+           
